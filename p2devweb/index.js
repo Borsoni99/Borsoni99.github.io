4ff081ba3c004039cfe520136c7034df083b226d
Borsoni <40211721+Borsoni99@users.noreply.github.com>
2023-11-16 23:00:31 -0300

    Add files via upload

---
+++ p2devweb/index.js
@@ -0,0 +1,15 @@
+function submitForm() {
+    var senhaInput = document.getElementById('senha');
+    var senha = senhaInput.value;
+    var senhamd5 = "81dc9bdb52d04dc20036dbd8313ed055";
+    var inputMD5 = hex_md5(senha)
+
+    if (inputMD5 == senhamd5) {
+        sessionStorage.setItem('auth', 'autorizado');
+        document.location = "/atletas/atletas.html";
+    } else {
+        alert("senha incorreta!");
+    }
+}
+
+
